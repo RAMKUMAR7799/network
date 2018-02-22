0db417a5f9b359bb37ad0e0b8298ef39b40a6ac2
RAMKUMAR7799 <ramaselvanram@gmail.com>
2018-02-22 16:33:18 +0530

    Create pos.py

---
+++ pos.py
@@ -0,0 +1,8 @@
+num=input("Entre your number")
+n=int(num)
+if n == 0:
+  print("The number {0} is Zero".format(n))
+elif n > 0:
+  print("The number {0} is Positive".format(n))
+elif n < 0:
+  print("The number {0} is Negaitive".format(n))
